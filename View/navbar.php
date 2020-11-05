cbd400ceae54e01e837a83c2abd668ff4cae3751
Antonio Jennings <Jenningsantonio17@gmail.com>
2020-11-05 15:27:32 -0500

    MVC Model

---
+++ View/navbar.php
@@ -0,0 +1,26 @@
+<!-- Navbar -->
+<div class='navbar-grid'>
+            <!-- Navbar Left -->
+                <div class='navbar-left'>
+                    <h1>Learning Experience</h1>
+                    <img class='bee-pic' src='./View/Public/Images/bee.png'>   
+                </div>
+            <!-- Navbar Left -->
+
+            <!-- Navbar Menu -->
+                <div class='navbar-menu'>
+                    <h3>Math</h3>
+                    <h3>Language Arts</h3>
+                    <h3>Social Studies</h3>
+                    <h3>Science</h3>
+                </div>
+            <!-- Navbar Menu -->
+
+            <!-- Navbar Right -->
+                <div class='navbar-right'>
+                    <button class='btn-btn btn btn-primary'>Sign up</button>
+                    <button class='btn-btn btn btn-primary'>Try For Free</button>
+                </div>
+            <!-- Navbar Right -->
+        </div>
+<!-- Navbar-->
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+    include './View/header.php';
+    include './View/navbar.php';
+?>
+
+<!-- Introduction -->
+    <div class='intro-grid'>
+        <img class='banner-pic' src='./View/Public/Images/Banner.jpg'>
+        <p>Allow your kids to explore and learn new things!</p>
+    </div>
+<!-- Introduction -->
+
+<!-- Information Cards -->
+    <div class='information-grid'>
+        <!-- Info 1 -->
+            <div class='info-one-grid'>
+                <p>Counting numbers, rhyming words,
+                    learning letters, and more!</p>
+                
+                <ul class='remove-style'>
+                    <li>Math</li>
+                    <li>Language Arts</li>
+                </ul>
+            </div>
+        <!-- Info 1 -->
+
+        <!-- Info 2 -->
+            <div class='info-two-grid'>
+                <p>Basic and advance understanding 
+                    of different subjects!</p>
+                
+                <ul class='remove-style'>
+                    <li>Math</li>
+                    <li>Language Arts</li>
+                    <li>Social Studies</li>
+                    <li>Science</li>
+                </ul>
+            </div>
+        <!-- Info 2 -->
+    </div>
+<!-- Information Cards -->
+
+<?php
+    include './View/footer.php';
+?>
